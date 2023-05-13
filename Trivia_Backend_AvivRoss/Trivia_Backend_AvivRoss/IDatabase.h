07c80103662cf591c1e9244c02857d5fa10bfbdc
Aviv <you@example.com>
2023-05-13 12:48:48 +0300

    added sqlite library

---
+++ Trivia_Backend_AvivRoss/Trivia_Backend_AvivRoss/IDatabase.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+class IDatabase
+{
+public:
+	virtual bool open() = 0;
+	virtual bool close() = 0;
+	virtual int doesUserExist(string userName) = 0;
+	virtual int doesPasswordMatch(string pass, string pass2) = 0;
+	virtual int addNewUser(string username, string pass, string email) = 0;
+};
