174290f7e6281e486154124da7718f6faa257660
Aviv <you@example.com>
2023-05-15 01:30:01 +0300

    fixed serializer class not adding headers to the messeges.

--- Trivia_Backend_AvivRoss/Trivia_Backend_AvivRoss/Consts.h
+++ Trivia_Backend_AvivRoss/Trivia_Backend_AvivRoss/Consts.h
@@ -10,6 +10,7 @@ typedef unsigned char byte;
 typedef std::vector<byte> Buffer;
 
 #define DEBUG_MODE true // on or off debug prints
+#define ERROR_CODE 256
 #define LOGIN_CODE 1
 #define SIGN_UP_CODE 2
 
